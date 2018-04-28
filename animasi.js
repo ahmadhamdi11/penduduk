08a05132e0302187c63549837f5109022834b292
sasaara88 <sasaara88@gmail.com>
2018-04-28 13:55:02 +0700

    commit kelima

--- animasi.js
+++ animasi.js
@@ -3,9 +3,11 @@
 		$(window).scroll(function(){
 			if($(window).scrollTop()>100){
 				$('#navAn').css("height", "50px").css("padding-top", "0px");
+				$('#garuda').css("height", "30px")
 				
 			}else{
 				$('#navAn').css("height", "100px").css("padding-top", "20px");
+				$('#garuda').css("height", "50px");
 			}
 		});
 
