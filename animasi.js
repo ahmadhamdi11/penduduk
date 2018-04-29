e73416f0b34ccfc8f7c5ceff78087d7e0bba6e6e
ahmadhamdi11 <ahmadhamdi112@yahoo.com>
2018-04-29 07:30:16 +0700

    tambah marquee

--- animasi.js
+++ animasi.js
@@ -1,13 +1,15 @@
 	$(document).ready(function(){
 		
 		$(window).scroll(function(){
-			if($(window).scrollTop()>100){
+			if($(window).scrollTop()>250){
 				$('#navAn').css("height", "50px").css("padding-top", "0px");
 				$('#garuda').css("height", "30px").css("width", "auto");
+				$('#judul').fadeIn();
 				
 			}else{
 				$('#navAn').css("height", "100px").css("padding-top", "20px");
 				$('#garuda').css("height", "50px").css("width","auto");
+				$('#judul').fadeOut();
 			}
 		});
 
